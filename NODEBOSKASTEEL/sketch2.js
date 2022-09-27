39a27a82a35ae8b3914a31cd57cc678c47aabc06
Roman Willems <romanw@hotmail.com>
2022-09-27 12:13:41 +0200

    sketch tests

--- NODEBOSKASTEEL/sketch2.js
+++ NODEBOSKASTEEL/sketch2.js
@@ -1,19 +1,26 @@
+function setup() {   
+  createCanvas(800, 400);
+  //left canvas will be display area for story text	
+	leftCanvas = createGraphics(400,400);
+	//rightCanvas will be display are for story animation
+	rightCanvas = createGraphics(400,400);
 
-let mySound;
-function preload() {
-  soundFormats('mp3', 'ogg');
-  mySound = loadSound('assets/doorbell');
 }
 
-function setup() {
-  let cnv = createCanvas(100, 100);
-  cnv.mousePressed(canvasPressed);
-  background(220);
-  text('tap here to play', 10, 20);
+function draw() {
+  drawRightCanvas();
+	image(rightCanvas, 400, 0);
+  drawLeftCanvas();
+  image(leftCanvas, 0, 0);
+  
 }
-
-function canvasPressed() {
-  // playing a sound file on a user gesture
-  // is equivalent to `userStartAudio()`
-  mySound.play();
+//function to draw elements in the right canvas 
+function drawRightCanvas() {
+  rightCanvas.background(100, 100, 50);
+  rightCanvas.fill(0,0,0);
+}
+//function to draw elements in the left canvas 
+function drawLeftCanvas() {
+  leftCanvas.background(200,0,0);
+  leftCanvas.fill(0,0,0);
 }
