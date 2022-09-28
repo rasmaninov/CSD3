42d8e66658ec3be1a1851349f2ad4c413f061e1c
Rahsaan Bleijs <rahsaan@live.nl>
2022-09-28 12:12:43 +0200

    fakke

--- NODEBOSKASTEEL/bal0/sketch.js
+++ NODEBOSKASTEEL/bal0/sketch.js
@@ -1,8 +1,97 @@
-function setup(){
-  createCanvas(400,400);
+let yoff = 0;
+let sound = false;
+let mic;
+let bc = 0;
+let amount = 2000;
+let xoff = 02;
+let radius;
+let startRadius = 750;
+let modDepth;
+let movingA = [];
+let movingSize = 1250;
+
+function setup() {
+  let cnv = createCanvas(windowWidth, windowHeight);
+    cnv.touchStarted(userStartAudio);
+    cnv.mousePressed(userStartAudio);
+
+  mic = new p5.AudioIn();
+  mic.start();
+  // movingA.length = 50;
+  for(i = 0; i < movingSize; i++){
+    movingA.push(0);
+  }
+  // frameRate(1);
 }
 
-function draw(){
-  fill('red');
-  ellipse(200,200,20,20);
+
+
+function draw() {
+  let xStart, yStart;
+  let sum = 0;
+
+  micLevel = mic.getLevel() * 2;
+  if (micLevel > 1){
+    micLevel = 1;
+  }
+  movingA.push(micLevel);
+  movingA.shift();
+  // console.log(movingA);
+  for(i = 0; i < movingA.length; i++){
+    sum += movingA[i];
+  }
+  modDepth = (sum / movingA.length) * 2;
+  if (modDepth > 1){
+    modDepth = 1;
+  }
+  // modDepth = 0;
+  console.log('mod ' + modDepth);
+  //
+  radius = startRadius + (startRadius * modDepth);
+  // radius = startRadius;
+  console.log('radius ' + radius);
+
+  stroke(0);
+  strokeWeight(1);
+  fill(255,40);
+  // fill(0);
+  translate(width/2, height/2);
+  beginShape();
+  for( i = 0.0 ; i <= TWO_PI; i += TWO_PI / amount){
+    noiseV = map(noise(cos(i)+1*modDepth, sin(i)+1*modDepth, yoff), 0.0, 1, 0, micLevel);
+    // a = cos(-i) * radius * (micLevel + 1);
+    // b = sin(-i) * radius * (micLevel + 1);
+    //
+    // a = a * (noiseV +1);
+    // b = b * (noiseV +1);
+    a = cos(-i) * radius * noiseV;
+    b = sin(-i) * radius * noiseV;
+
+
+    vertex(a,b);
+  }
+  endShape(CLOSE);
+  // console.log('noiseV' + noiseV);
+
+
+
+  yoff += 0.005;
+
+}
+
+function touchStarted(){
+  if(sound){
+    sound = false;
+    console.log('stop');
+    mic.stop();
+
+    // bc = 255;
+  } else if(!sound){
+    // background(255);
+    sound = true;
+    console.log('start');
+    mic.start();
+    // bc = 0;
+
+  }
 }
